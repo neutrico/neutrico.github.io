6e50f2ba661e16ceaa859ba29ced025c8f5268b1
Marcin Antczak <marcin.antczak@neutrico.eu>
2016-07-01 17:49:56 +0200

    init

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+for pkg in "async" "babel-cli" "babel-preset-es2015" "babel-preset-stage-2" "browser-sync" "cli-color" "cssnano" "fs-extra" "node-sass" "postcss-cli" "postcss-uncss" "postcss"
+do
+    npm i $pkg --save-dev
+done
