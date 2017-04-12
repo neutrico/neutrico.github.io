13c6d9d147065f0927e4b364f3349ed0c0809b01
Marcin Antczak <marcin.antczak@neutrico.eu>
2017-04-12 08:49:01 +0200

    nojekyll

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+
+
+    render() {
+        const Title = styled.h1`
+background-color: red;
+`;
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
