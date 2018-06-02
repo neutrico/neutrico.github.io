919b86587788d66514673e80af004812321c8c22
Marcin Antczak <marcin.antczak@neutrico.eu>
2018-06-02 03:47:37 +0200

    reset cname

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './App';
+import Store from './store';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+const StoreInstance = Store();
+
+render(
+  <Provider store={StoreInstance}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
